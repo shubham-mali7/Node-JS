1ab82cca95f3da3bbd0b62b8f2b2d48fafb83443
Shubham Mali <shubhammali31299@gmail.com>
2023-01-10 14:21:40 +0530

    package.json

---
+++ index5.js
@@ -0,0 +1,8 @@
+var colors = require("colors");
+console.log("Shubham".red.bgBlack);
+console.log("Sanjay".green);
+console.log("Mali".yellow.bgMagenta);
+
+// import chalk from "chalk";
+
+// console.log(chalk.blue("Hello world!"));
